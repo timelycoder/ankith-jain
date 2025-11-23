3a7ae072a82dabfb2892c14df591988e7d8f7af4
sumon <sms.sumon@gmail.com>
2025-11-24 00:46:10 +0600

    added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,74 @@
 import React from "react";
+import { FaEnvelope, FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";
 
 export default function Contact() {
   return (
     <div>
       <div className="grid grid-cols-[1fr_3fr] gap-4">
         {/* First column = 1/4 */}
-        <div className="bg-gray-100 p-4 rounded shadow">
-          First Column (1/4 width)
+        <div>
+          <div className="bg-gray-100 p-4 rounded shadow">
+            First Column (1/4 width)
+            <div className="card bg-base-100 shadow-sm grid grid-cols-4">
+              {/* Left Column (1/4) */}
+              <div className="col-span-1 flex justify-center items-center">
+                <FaEnvelope className="w-8 h-8 text-primary" />
+              </div>
+
+              {/* Right Column (3/4) */}
+              <div className="card-body col-span-3">
+                <h2 className="card-title">MAIL US</h2>
+                <p>info@bluebase.com</p>
+                <p>info@bluebase2.com</p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-100 p-4 rounded shadow">
+            <div className="card bg-base-100 shadow-sm grid grid-cols-4">
+              {/* Left Column (1/4) */}
+              <div className="col-span-1 flex justify-center items-center">
+                <FaPhoneAlt className="w-8 h-8 text-primary" />
+              </div>
+
+              {/* Right Column (3/4) */}
+              <div className="card-body col-span-3">
+                <h2 className="card-title">MAIL US</h2>
+                <p>info@bluebase.com</p>
+                <p>info@bluebase2.com</p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-100 p-4 rounded shadow">
+            <div className="card bg-base-100 shadow-sm grid grid-cols-4">
+              {/* Left Column (1/4) */}
+              <div className="col-span-1 flex justify-center items-center">
+                <FaMapMarkerAlt className="w-8 h-8 text-primary" />
+              </div>
+
+              {/* Right Column (3/4) */}
+              <div className="card-body col-span-3">
+                <h2 className="card-title">MAIL US</h2>
+                <p>info@bluebase.com</p>
+                <p>info@bluebase2.com</p>
+              </div>
+            </div>
+          </div>
+          {/* ///////////// */}
+          <div className="bg-gray-100 p-4 rounded shadow">
+            <div className="card bg-base-100 shadow-sm grid grid-row-2">
+              {/* Left Column (1/4) */}
+              <div className="col-span-1 flex justify-center items-center">
+                <h2 className="card-title">MAIL US</h2>
+              </div>
+
+              {/* Right Column (3/4) */}
+              <div className="card-body col-span-3">
+                <FaMapMarkerAlt className="w-8 h-8 text-primary" />
+                <FaMapMarkerAlt className="w-8 h-8 text-primary" />
+                <FaMapMarkerAlt className="w-8 h-8 text-primary" />
+              </div>
+            </div>
+          </div>
         </div>
 
         {/* Second column = 3/4 */}
